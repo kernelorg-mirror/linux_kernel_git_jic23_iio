289f3cfe9a51e7493402989d2951a01343d60f2c
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-10-31 11:07:53 +0000

    staging:iio:cdc:ad7746: Remove unused includes and add missing ones.
    Based on manual inspection of iwyu tool output.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/cdc/ad7746.c
+++ drivers/staging/iio/cdc/ad7746.c
@@ -5,15 +5,23 @@
  * Copyright 2011 Analog Devices Inc.
  */
 
+#include <linux/bits.h>
+#include <linux/cache.h>
+#include <linux/errno.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/i2c.h>
-#include <linux/interrupt.h>
 #include <linux/kernel.h>
+#include <linux/kstrtox.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
-#include <linux/slab.h>
-#include <linux/stat.h>
+#include <linux/mutex.h>
+#include <linux/property.h>
+#include <linux/stddef.h>
 #include <linux/sysfs.h>
+#include <linux/types.h>
+
+#include <asm/byteorder.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
