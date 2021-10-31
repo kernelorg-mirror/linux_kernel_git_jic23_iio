7cc9f7fab4a80e6df7f1498fee9560b4e60f0526
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-10-31 11:37:09 +0000

    staging:iio:resolver:ads1210: Remove unused and add missing includes.
    Also forwards definitions where that was all that was needed.
    Done by manual inspection of the results of the iwyu tool.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/resolver/ad2s1210.c
+++ drivers/staging/iio/resolver/ad2s1210.c
@@ -4,18 +4,31 @@
  *
  * Copyright (c) 2010-2010 Analog Devices Inc.
  */
-#include <linux/types.h>
-#include <linux/mutex.h>
-#include <linux/device.h>
-#include <linux/spi/spi.h>
-#include <linux/slab.h>
-#include <linux/sysfs.h>
+
+#include <linux/bits.h>
+#include <linux/cache.h>
 #include <linux/delay.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/errno.h>
 #include <linux/gpio/consumer.h>
+#include <linux/kernel.h>
+#include <linux/kstrtox.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/spi/spi.h>
+#include <linux/stddef.h>
+#include <linux/sysfs.h>
+#include <linux/types.h>
+
+#include <asm/byteorder.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+
+struct gpio_desc;
 
 #define DRV_NAME "ad2s1210"
 
