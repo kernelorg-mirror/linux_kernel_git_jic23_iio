c0e6e54d327dc5ea91c6a0573680b199aceb2593
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-11-01 20:52:12 +0000

    iio:accel:da280: Switch from CONFIG_PM_SLEEP guards to __maybe_unused
    For cases where we are using SIMPLE_DEV_PM() it is preferred to
    just mark the PM function __maybe_unused rather than have #ifdef based
    guards.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/da280.c
+++ drivers/iio/accel/da280.c
@@ -8,6 +8,7 @@
 
 #include <linux/acpi.h>
 #include <linux/bits.h>
+#include <linux/compiler.h>
 #include <linux/device.h>
 #include <linux/errno.h>
 #include <linux/i2c.h>
@@ -158,17 +159,15 @@ static int da280_probe(struct i2c_client *client,
 	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int da280_suspend(struct device *dev)
+static __maybe_unused int da280_suspend(struct device *dev)
 {
 	return da280_enable(to_i2c_client(dev), false);
 }
 
-static int da280_resume(struct device *dev)
+static __maybe_unused int da280_resume(struct device *dev)
 {
 	return da280_enable(to_i2c_client(dev), true);
 }
-#endif
 
 static SIMPLE_DEV_PM_OPS(da280_pm_ops, da280_suspend, da280_resume);
 
