4f8019db7771805b6a5eb28f7a12724b1b7d6071
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-11-08 20:08:55 +0000

    iio:temperature:tmp006: Switch from PM CONFIG guards to __maybe_used
    Letting the compiler remove these functions is less error prone so
    I want to encourange this approach in IIO.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/temperature/tmp006.c
+++ drivers/iio/temperature/tmp006.c
@@ -261,17 +261,15 @@ static int tmp006_probe(struct i2c_client *client,
 	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int tmp006_suspend(struct device *dev)
+static __maybe_unused int tmp006_suspend(struct device *dev)
 {
 	return tmp006_power(dev, false);
 }
 
-static int tmp006_resume(struct device *dev)
+static __maybe_unused int tmp006_resume(struct device *dev)
 {
 	return tmp006_power(dev, true);
 }
-#endif
 
 static SIMPLE_DEV_PM_OPS(tmp006_pm_ops, tmp006_suspend, tmp006_resume);
 
