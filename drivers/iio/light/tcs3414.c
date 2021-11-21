added17c48f80d516dc46a63bbd4c2080baa2187
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-11-21 18:13:47 +0000

    iio:light:tcs3414: Switch from CONFIG_PM guards to pm_ptr() / __maybe_unused
    Letting the compiler remove these functions when the kernel is built
    without CONFIG_PM support is simpler and less error prone than the
    use of #ifdef based config guards.
    
    Removing instances of this approach from IIO also stops them being
    copied into new drivers.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/light/tcs3414.c
+++ drivers/iio/light/tcs3414.c
@@ -345,22 +345,20 @@ static int tcs3414_probe(struct i2c_client *client,
 	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int tcs3414_suspend(struct device *dev)
+static __maybe_unused int tcs3414_suspend(struct device *dev)
 {
 	struct tcs3414_data *data = iio_priv(i2c_get_clientdata(
 		to_i2c_client(dev)));
 	return tcs3414_powerdown(data);
 }
 
-static int tcs3414_resume(struct device *dev)
+static __maybe_unused int tcs3414_resume(struct device *dev)
 {
 	struct tcs3414_data *data = iio_priv(i2c_get_clientdata(
 		to_i2c_client(dev)));
 	return i2c_smbus_write_byte_data(data->client, TCS3414_CONTROL,
 		data->control);
 }
-#endif
 
 static SIMPLE_DEV_PM_OPS(tcs3414_pm_ops, tcs3414_suspend, tcs3414_resume);
 
@@ -373,7 +371,7 @@ MODULE_DEVICE_TABLE(i2c, tcs3414_id);
 static struct i2c_driver tcs3414_driver = {
 	.driver = {
 		.name	= TCS3414_DRV_NAME,
-		.pm	= &tcs3414_pm_ops,
+		.pm	= pm_ptr(&tcs3414_pm_ops),
 	},
 	.probe		= tcs3414_probe,
 	.id_table	= tcs3414_id,
