3cd6aad78581dbd939243afef221049ae8c4bef5
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-11-21 18:00:17 +0000

    iio:light:apds9300: Switch from CONFIG_PM guards to pm_ptr() / __maybe_unused
    Letting the compiler remove these functions when the kernel is built
    without CONFIG_PM support is simpler and less error prone than the
    use of #ifdef based config guards.
    
    Removing instances of this approach from IIO also stops them being
    copied into new drivers.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/light/apds9300.c
+++ drivers/iio/light/apds9300.c
@@ -466,8 +466,7 @@ static int apds9300_remove(struct i2c_client *client)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int apds9300_suspend(struct device *dev)
+static __maybe_unused int apds9300_suspend(struct device *dev)
 {
 	struct iio_dev *indio_dev = i2c_get_clientdata(to_i2c_client(dev));
 	struct apds9300_data *data = iio_priv(indio_dev);
@@ -480,7 +479,7 @@ static int apds9300_suspend(struct device *dev)
 	return ret;
 }
 
-static int apds9300_resume(struct device *dev)
+static __maybe_unused int apds9300_resume(struct device *dev)
 {
 	struct iio_dev *indio_dev = i2c_get_clientdata(to_i2c_client(dev));
 	struct apds9300_data *data = iio_priv(indio_dev);
@@ -494,10 +493,6 @@ static int apds9300_resume(struct device *dev)
 }
 
 static SIMPLE_DEV_PM_OPS(apds9300_pm_ops, apds9300_suspend, apds9300_resume);
-#define APDS9300_PM_OPS (&apds9300_pm_ops)
-#else
-#define APDS9300_PM_OPS NULL
-#endif
 
 static const struct i2c_device_id apds9300_id[] = {
 	{ APDS9300_DRV_NAME, 0 },
@@ -509,7 +504,7 @@ MODULE_DEVICE_TABLE(i2c, apds9300_id);
 static struct i2c_driver apds9300_driver = {
 	.driver = {
 		.name	= APDS9300_DRV_NAME,
-		.pm	= APDS9300_PM_OPS,
+		.pm	= pm_ptr(&apds9300_pm_ops),
 	},
 	.probe		= apds9300_probe,
 	.remove		= apds9300_remove,
