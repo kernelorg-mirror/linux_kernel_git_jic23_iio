76ec1a1b7c32e7ef62fcde6bfed592c5618735f7
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-11-21 17:52:28 +0000

    iio:adc:vf610: Switch from CONFIG_PM guards to pm_ptr() / __maybe_unused
    Letting the compiler remove these functions when the kernel is built
    without CONFIG_PM support is simpler and less error prone than the
    use of ifdef based config guards.
    
    Removing instances of this approach from IIO also stops them being
    copied into new drivers.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>

--- drivers/iio/adc/vf610_adc.c
+++ drivers/iio/adc/vf610_adc.c
@@ -912,8 +912,7 @@ static int vf610_adc_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int vf610_adc_suspend(struct device *dev)
+static __maybe_unused int vf610_adc_suspend(struct device *dev)
 {
 	struct iio_dev *indio_dev = dev_get_drvdata(dev);
 	struct vf610_adc *info = iio_priv(indio_dev);
@@ -930,7 +929,7 @@ static int vf610_adc_suspend(struct device *dev)
 	return 0;
 }
 
-static int vf610_adc_resume(struct device *dev)
+static __maybe_unused int vf610_adc_resume(struct device *dev)
 {
 	struct iio_dev *indio_dev = dev_get_drvdata(dev);
 	struct vf610_adc *info = iio_priv(indio_dev);
@@ -952,7 +951,6 @@ disable_reg:
 	regulator_disable(info->vref);
 	return ret;
 }
-#endif
 
 static SIMPLE_DEV_PM_OPS(vf610_adc_pm_ops, vf610_adc_suspend, vf610_adc_resume);
 
@@ -962,7 +960,7 @@ static struct platform_driver vf610_adc_driver = {
 	.driver         = {
 		.name   = DRIVER_NAME,
 		.of_match_table = vf610_adc_match,
-		.pm     = &vf610_adc_pm_ops,
+		.pm     = pm_ptr(&vf610_adc_pm_ops),
 	},
 };
 
