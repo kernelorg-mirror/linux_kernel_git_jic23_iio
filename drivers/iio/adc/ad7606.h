9efedb3ebb7b3bedd2ce3fd06b954e5843488291
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2022-05-02 15:38:47 +0100

    iio: adc: ad7606: Fix alignment for DMA safety
    ____cacheline_aligned is an insufficient guarantee for non-coherent DMA
    on platforms with 128 byte cachelines above L1.  Switch to the updated
    IIO_ALIGN definition.
    
    Fixes: 7989b4bb23fe ("iio: adc: ad7616: Add support for AD7616 ADC")
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/ad7606.h
+++ drivers/iio/adc/ad7606.h
@@ -120,7 +120,7 @@ struct ad7606_state {
 	 * transfer buffers to live in their own cache lines.
 	 * 16 * 16-bit samples + 64-bit timestamp
 	 */
-	unsigned short			data[20] ____cacheline_aligned;
+	unsigned short			data[20] __aligned(IIO_ALIGN);
 	__be16				d16[2];
 };
 
